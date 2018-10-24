88b87c48e3865efb7513c53e691701b56a44f234
Unknown <fkmontero9@gmail.com>
2018-10-24 10:08:10 +0200

    P6 2.0

--- Practica6_Intents_explicito/app/src/main/java/com/example/alumno/practica6_intents_explicito/MainActivity.java
+++ Practica6_Intents_explicito/app/src/main/java/com/example/alumno/practica6_intents_explicito/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.alumno.practica6_intents_explicito;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Declaro las variables tipo Button de la app ventana principal
+        Button buttonAlta = (Button) findViewById(R.id.buttonAlta);
+        Button buttonModf = (Button) findViewById(R.id.buttonMod);
+
+        //Pulsar botón de Altas
+        buttonAlta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Se llama a la seginda actividad y se le manda el boton pulsado.
+                String accion="Alta";
+                Intent i = new Intent(MainActivity.this,SegundaActivity.class);
+                i.putExtra("boton", accion);
+                startActivityForResult(i,SECONDARY_ACTIVITY_TAG);
+            }
+        });
+
+
     }
+    /**
+     * "Etiqueta" que usamos para llamar a la segunda actividad, y que
+     * esperamos recibir como primer parámetro de vuelta en el
+     * callback onActivityResult().
+     */
+    private static final int SECONDARY_ACTIVITY_TAG = 1;
 }
--- Practica6_Intents_explicito/app/src/main/java/com/example/alumno/practica6_intents_explicito/SegundaActivity.java
+++ Practica6_Intents_explicito/app/src/main/java/com/example/alumno/practica6_intents_explicito/SegundaActivity.java
@@ -1,4 +1,46 @@
 package com.example.alumno.practica6_intents_explicito;
 
-public class SegundaActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SegundaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.segunda_activity);
+
+        //Declaro la variable tipo Button de la app ventana secundaria
+        Button buttonAceptar = (Button) findViewById(R.id.buttonAcept);
+
+        //Alta o Modifica dependiendo de la acción pulsada
+        String boton = getIntent().getStringExtra("boton");
+
+        switch (boton){
+            case "Alta": //Pulsar boton de Altas
+                buttonAceptar.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    //Declaramos variables de los EditTexts del nombre y del apelido
+                    EditText nombre = (EditText) findViewById(R.id.editTextNombre);
+                    EditText apellido = (EditText) findViewById(R.id.editTextApell);
+
+                    //Compruebo que no se deja en blanco los EditText
+                    if(nombre.getText().toString().equals("") || apellido.getText().toString().equals("")) {
+                        Toast toast = Toast.makeText(getApplicationContext(), "Rellena todos los campos.", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }else{ //Caso contrario, lo rellena correctamente, devolvemos los valores de los campos a la main activity
+
+                    }
+            }
+        });
+                break;
+            case "Modf": //Pulsar boton de Modificar
+
+                break;
+        }
+    }
 }
